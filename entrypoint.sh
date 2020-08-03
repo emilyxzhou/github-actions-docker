c4fe0abc3582a05a8b16dd69800cebf7caff2a1b
Emily Zhou <zhouxemily@gmail.com>
2020-08-03 16:46:42 -0700

    Fix 'source not found' error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /root/catkin_ws
-source /opt/ros/kinetic/setup.bash && catkin_make
+/bin/bash -c "source /opt/ros/kinetic/setup.bash " && catkin_make
 rostest ros_messaging test_message_publisher.test
